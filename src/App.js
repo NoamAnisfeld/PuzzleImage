e08aa552b8199a8ec7ebd5ff3f5e76d11290abf1
Noam Anisfeld <NoamAnisfeld@gmail.com>
2022-05-25 13:42:39 +0300

    extracting the settings panel to a separate module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import { useState, useEffect, useRef } from 'react';
+import { defaultSettings, SettingsPanel } from './SettingsPanel.js';
 
 // ***** Debugging Utilities *****
 const debugConsole = document.getElementById("debug-console");
@@ -27,7 +28,7 @@ function debugLog(...args) {
 }
 
 // ***** App *****
-const ROUND_FACTOR = 10;
+// const ROUND_FACTOR = 10;
 
 function factorRound(num, factor) {
 	return Math.round(num / factor) * factor;
@@ -41,13 +42,6 @@ function factorCeil(num, factor) {
 	return num - (num % factor) + factor;
 }
 
-const defaultSettings = {
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/%D0%A0%D0%B0%D0%BD%D0%BE%D0%BA_%D0%BD%D0%B0_%D0%9C%D0%B0%D0%BD%D0%B3%D1%83%D0%BF%D1%96.jpg/750px-%D0%A0%D0%B0%D0%BD%D0%BE%D0%BA_%D0%BD%D0%B0_%D0%9C%D0%B0%D0%BD%D0%B3%D1%83%D0%F%D1%96.jpg",
-		imageWidth: Math.max(300, window.innerWidth * 0.5),
-		rows: 3,
-		cols: 2
-	};
 // defaultSettings.imageWidth = factorRound(
 // 	defaultSettings.imageWidth,
 // 	ROUND_FACTOR * defaultSettings.cols
@@ -87,83 +81,6 @@ const defaultSettings = {
 // 		);
 // 	}
 // }
-
-/*
- * @prop startGameCallback {function(settings:{object})}
- */
-function SettingsPanel(props) {
-	const [imageUrl, setImageUrl] = useState(defaultSettings.imageUrl),
-		[imageAspectRatio, setImageAspectRatio] = useState(null),
-		[rows, setRows] = useState(defaultSettings.rows),
-		[cols, setCols] = useState(defaultSettings.cols),
-		[isClosing, setIsClosing] = useState(false);
-
-	function handleChangeRows(event) {
-		const n = Number(event.target.value);
-
-		if (n > 0 && Number.isInteger(n)) {
-			setRows(n);
-		}
-	}
-
-	function handleChangeCols(event) {
-		const n = Number(event.target.value);
-
-		if (n > 0 && Number.isInteger(n)) {
-			setCols(n);
-		}
-	}
-
-	function handleChangeImage() {
-		const newUrl = prompt("הכנס כתובת של תמונה");
-		if (newUrl) {
-			setImageUrl(newUrl);
-			setImageAspectRatio(null); // perhaps don't to if it's the same URL as before because it wouldn't onLoad?
-		}
-	}
-
-	function handleStartGame() {
-		setIsClosing(true);
-
-		props.handleStartGame({
-			imageUrl: imageUrl,
-			imageAspectRatio: imageAspectRatio,
-			rows: rows,
-			cols: cols
-		});
-	}
-
-	return (
-		<div id="settings-panel" className={isClosing ? "goaway-up" : null}>
-			<form id="controls">
-				<label>
-					שורות:
-					<input type="number" value={rows} onChange={handleChangeRows} />
-				</label>
-				<label>
-					עמודות:
-					<input type="number" value={cols} onChange={handleChangeCols} />
-				</label>
-				<button type="button" onClick={handleChangeImage}>
-					החלפת תמונה
-				</button>
-				<button
-					type="button"
-					onClick={handleStartGame}
-					disabled={!imageAspectRatio}
-				>
-					צור פאזל
-				</button>
-			</form>
-			<img
-				src={imageUrl}
-				onLoad={(e) =>
-					setImageAspectRatio(e.target.naturalWidth / e.target.naturalHeight)
-				}
-			/>
-		</div>
-	);
-}
 
 /*
  * @prop width {integer}
@@ -457,7 +374,6 @@ function App(props) {
 				"--height": `${imageHeight}px`,
 				"--rows": rows,
 				"--cols": cols,
-				"--ROUND_FACTOR": ROUND_FACTOR
 			}}
 		>
 			<SettingsPanel handleStartGame={handleStartGame} />
---
+++ src/SettingsPanel.js
@@ -0,0 +1,89 @@
+import { useState, useEffect, useRef } from 'react';
+
+const defaultSettings = {
+    imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/%D0%A0%D0%B0%D0%BD%D0%BE%D0%BA_%D0%BD%D0%B0_%D0%9C%D0%B0%D0%BD%D0%B3%D1%83%D0%BF%D1%96.jpg/750px-%D0%A0%D0%B0%D0%BD%D0%BE%D0%BA_%D0%BD%D0%B0_%D0%9C%D0%B0%D0%BD%D0%B3%D1%83%D0%F%D1%96.jpg",
+    imageWidth: Math.max(300, window.innerWidth * 0.5),
+    rows: 3,
+    cols: 2
+};
+
+/*
+ * @prop startGameCallback {function(settings:{object})}
+ */
+function SettingsPanel(props) {
+    console.log('test');
+	const [imageUrl, setImageUrl] = useState(defaultSettings.imageUrl),
+		[imageAspectRatio, setImageAspectRatio] = useState(null),
+		[rows, setRows] = useState(defaultSettings.rows),
+		[cols, setCols] = useState(defaultSettings.cols),
+		[isClosing, setIsClosing] = useState(false);
+
+	function handleChangeRows(event) {
+		const n = Number(event.target.value);
+
+		if (n > 0 && Number.isInteger(n)) {
+			setRows(n);
+		}
+	}
+
+	function handleChangeCols(event) {
+		const n = Number(event.target.value);
+
+		if (n > 0 && Number.isInteger(n)) {
+			setCols(n);
+		}
+	}
+
+	function handleChangeImage() {
+		const newUrl = prompt("הכנס כתובת של תמונה");
+		if (newUrl) {
+			setImageUrl(newUrl);
+			setImageAspectRatio(null); // perhaps don't to if it's the same URL as before because it wouldn't onLoad?
+		}
+	}
+
+	function handleStartGame() {
+		setIsClosing(true);
+
+		props.handleStartGame({
+			imageUrl: imageUrl,
+			imageAspectRatio: imageAspectRatio,
+			rows: rows,
+			cols: cols
+		});
+	}
+
+	return (
+		<div id="settings-panel" className={isClosing ? "goaway-up" : null}>
+			<form id="controls">
+				<label>
+					שורות:
+					<input type="number" value={rows} onChange={handleChangeRows} />
+				</label>
+				<label>
+					עמודות:
+					<input type="number" value={cols} onChange={handleChangeCols} />
+				</label>
+				<button type="button" onClick={handleChangeImage}>
+					החלפת תמונה
+				</button>
+				<button
+					type="button"
+					onClick={handleStartGame}
+					disabled={!imageAspectRatio}
+				>
+					צור פאזל
+				</button>
+			</form>
+			<img
+				src={imageUrl}
+				onLoad={(e) =>
+					setImageAspectRatio(e.target.naturalWidth / e.target.naturalHeight)
+				}
+			/>
+		</div>
+	);
+}
+
+export { defaultSettings, SettingsPanel };
