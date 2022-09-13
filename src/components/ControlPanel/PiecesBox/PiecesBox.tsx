139867a1b9aca8320f4c98b5f09e3b661c4f1758
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-09-13 14:46:35 +0300

    temporarily switching pieces box to a button

--- src/components/ControlPanel/PiecesBox/PiecesBox.tsx
+++ src/components/ControlPanel/PiecesBox/PiecesBox.tsx
@@ -5,21 +5,26 @@ function PiecesBox({
 }: {
     pickPiece: (position: Position) => void
 }) {
-    return <svg
+    // return <svg
+    //     id="pieces-box"
+    //     width="100"
+    //     height="50"
+    //     // onClick={e => {
+    //     //     const {x, y}: Position = 
+    //     //         e.currentTarget.getBoundingClientRect();
+    //     //     pickPiece({x, y});
+    //     // }}
+    // >
+    //     <rect
+    //         width="100"
+    //         height="50"
+    //     />
+    // </svg>
+    return <button
         id="pieces-box"
-        width="100"
-        height="50"
-        // onClick={e => {
-        //     const {x, y}: Position = 
-        //         e.currentTarget.getBoundingClientRect();
-        //     pickPiece({x, y});
-        // }}
     >
-        <rect
-            width="100"
-            height="50"
-        />
-    </svg>
+        Get a piece
+    </button>
 }
 
 export default PiecesBox;
